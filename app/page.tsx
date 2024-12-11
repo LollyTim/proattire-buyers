6ddc9eac1e41524311678468ebd029ea8d0780ec
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-12-11 12:59:52 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=''>
+      <nav className='flex flex-row justify-between items-center h-6'>
+        <img src="/plogo.png" alt="logo" />
+
+        <a href="#" className=' text underline text-blue-50'>Register as a seller</a>
+      </nav>
+
+    </div>
+  )
+}
+
+export default page
